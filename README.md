52b54affda712e159acdf31cdd2c8221e3feb7f4
eduqg <eduardoqgomes@gmail.com>
2018-04-13 18:45:40 -0300

    Improving code and adding readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# Captura Mosquito
+
+## Instalação
+
+Clone this repository and enter on folder
+
+Create virtual environment and activate
+
+```console
+$ pip3 install virtualenv
+$ virtualenv venv
+$ source venv/bin/activate
+```
+
+To deactivate use
+
+```console
+$ deactivate
+```
+
+Install requirements
+
+```console
+pip3 install opencv-python Pillow
+```
+
+Run with
+
+```console
+$ python3 main.py
+```
+
+<p align="center">Grupo Projeto Captura Mosquito - Projeto Integrador 2<br /><br />
+<a href="https://fga.unb.br" target="_blank"><img width="200"src="https://4.bp.blogspot.com/-0aa6fAFnSnA/VzICtBQgciI/AAAAAAAARn4/SxVsQPFNeE0fxkCPVgMWbhd5qIEAYCMbwCLcB/s1600/unb-gama.png"></a>
+</p>
--- main.py
+++ main.py
@@ -1,90 +1,110 @@
 import os
 import cv2
+import glob
 from PIL import Image
 from subprocess import call
 
-def saveimages(faces, imagem):
+def print_menu():
+    print("Press one of the following commands")
+    print("  c - crop and save rectangles")
+    print("  r - resize cropped rectangles")
+    print("  Space - save entire picture")
+    print("  q - quit")
+
+# Saves rectangles from entire picture 
+def save_images(faces, imagem):
     img_counter = 0
     for (x, y, w, h) in faces:
             cv2.rectangle(imagem, (x, y), (x+w, y+h), (0, 255, 0), 4)
             crop_rectangle = imagem[y: y+h, x: x+w].copy()
             img_counter += 1
             img_name = "face_{}.png".format(img_counter)
-            cv2.imwrite("./Fotos/" + img_name, crop_rectangle)
-            print("{} written!".format(img_name))
+            cv2.imwrite("./Pictures/" + img_name, crop_rectangle)
+            print("{} written on Pictures folder!".format(img_name))
 
-def resizeimages(faces):
+# Resize to one specific size rectangles found
+def resize_images(faces):
     img_counter = 0
-    filelist=os.listdir('Fotos')
+    # Choose size
+    img_width = 100
+    img_height = 200
+    filelist=os.listdir('Pictures')
     for foto in filelist[:]: # filelist[:] makes a copy of filelist.
         if foto.endswith(".png"):
             img_counter += 1
-            resizename = "resize_{}.png".format(img_counter)
+            resize_name = "resize_{}.png".format(img_counter)
             img_name = "face_{}.png".format(img_counter)
-            img = Image.open("./Fotos/" + img_name)
-            img = img.resize((1000, 2000), Image.ANTIALIAS)
-            img.save('./Redimensionadas/' + resizename, 'PNG')
-            print("{} resized!".format(img_counter))
+            img = Image.open("./Pictures/" + img_name)
+            img = img.resize((img_width, img_height), Image.ANTIALIAS)
+            img.save('./ResizedPictures/' + resize_name, 'PNG')
+            print("{} written on Resized folder!".format(resize_name))
     print("All images resized!")
-
 
 arqCasc1 = 'haarcascade_frontalface_default.xml'
 arqCasc2 = 'haarcascade_eye.xml'
-faceCascade1 = cv2.CascadeClassifier(arqCasc1) #classificador para o rosto
-faceCascade2 = cv2.CascadeClassifier(arqCasc2) #classificador para os olhos
+# Face classifier
+faceCascade1 = cv2.CascadeClassifier(arqCasc1)
+# Eyes classifier
+faceCascade2 = cv2.CascadeClassifier(arqCasc2)
+webcam = cv2.VideoCapture(0)
 
-webcam = cv2.VideoCapture(0)  #instancia o uso da webcam
+# Clean older pictures
+files = glob.glob('Pictures/*.png')
+for f in files:
+    os.remove(f)
 
-img_counter = 0
+files = glob.glob('ResizedPictures/*.png')
+for f in files:
+    os.remove(f)
 
+print_menu()
 
 while True:
-    s, imagem = webcam.read() #pega efeticamente a imagem da webcam
-    imagem = cv2.flip(imagem,180) #espelha a imagem
+    img_counter = 0
+    s, imagem = webcam.read()
+    imagem = cv2.flip(imagem,180)
     ret, frame = webcam.read()
 
     faces = faceCascade1.detectMultiScale(
         imagem,
         minNeighbors=20,
         minSize=(30, 30),
-    maxSize=(300,300)
+        maxSize=(300,300)
     )
 
     olhos = faceCascade2.detectMultiScale(
         imagem,
         minNeighbors=20,
         minSize=(10, 10),
-    maxSize=(90,90)
+        maxSize=(90,90)
     )
 
-    # Desenha um retangulo nas faces e olhos detectados
+    # Draw rectangle on faces and eyes detected
     for (x, y, w, h) in faces:
         cv2.rectangle(imagem, (x, y), (x+w, y+h), (0, 255, 0), 4)
     for (x, y, w, h) in olhos:
         cv2.rectangle(imagem, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    cv2.imshow('Video', imagem) #mostra a imagem captura na janela
+    # Shows picture on screen
+    cv2.imshow('Video', imagem)
     
     k = cv2.waitKey(1)
 
     if k & 0xFF == ord('q'):
-        # O trecho seguinte e apenas para parar o codigo e fechar a janela
-        print("Escape hit, closing...")
+        # Close program
+        print("Closing...")
         break
     elif k%256 == 32:
-        # SPACE pressed
+        # Save picture hitting space
         img_name = "opencv_frame_{}.png".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
-        
     elif k & 0xFF == ord('c'):
         # crop faces
-        saveimages(faces, imagem)
-
+        save_images(faces, imagem)
     elif k & 0xFF == ord('r'):
         # Resize pictures
-        resizeimages(faces)
-        
+        resize_images(faces)
 
-webcam.release() #dispensa o uso da webcam
-cv2.destroyAllWindows() #fecha todas a janelas abertas
+webcam.release()
+cv2.destroyAllWindows()
